521eb95870e343cc514363edd2458e6b560e978e
jtme <janis@tilla.me>
2014-06-07 21:17:02 +0300

    DUH!

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
- 	
+ 
+  	//create a function to add item to list. 
+ 	function addtolist(a) {
+	  	var new_item = $("<li> <span class=\"item\">" + a + "</span></li>");
+	  	//dbug
+	  	var scrub = $.html(a).text();
+	  //	console.log(scrub)
+	  	//$( '<div>' ).html( foo ).text()
+		if(a.length>0)
+			{
+				new_item.appendTo('#list ul'); 		
+			}
+			else {
+				$('input').animate({'background-color': "#E56C75"}, 200);
+				$('input').animate({'background-color': "#A6BBC5"}, 100);
+			}
+		//RESET
+		$('input').val('');
+   }
+
  	var list_item = $('.item');
  	// turn item on and off 
 	list_item.on('click', function(){ $(this).parent().toggleClass("done"); });
@@ -12,20 +31,23 @@ $(document).ready(function() {
 			$('#input').click();
 		};
 	});	
-	//click to add items
 	  $('#input').click (function(){
-	  	var add_value = $('input').val();
-	  	var new_item = $("<li> <span class=\"item\">" + add_value + "</span></li>");
-		
-		if(add_value.length>0)
-			{
-							new_item.appendTo('#list ul'); 		
-			}
-			else {
-				$('input').animate({'background-color': "#E56C75"}, 200);
-				$('input').animate({'background-color': "#A6BBC5"}, 100);
-			}
-		$('input').val('');
+	  	
+
+				function splitString(stringToSplit, separator) {
+				  var arrayOfStrings = stringToSplit.split(separator);
+
+				  for (var i=0; i < arrayOfStrings.length; i++)
+				    console.log(arrayOfStrings[i]);
+					addtolist(arrayOfStrings[i]);
+				}
+
+				var origString = $('input').val()
+				var comma = ",";
+
+				splitString(origString, comma);
+	  	//addtolist($('input').val())
 	  });
-$('#list ul').sortable();
+	$('#list ul').sortable();
+
 })
